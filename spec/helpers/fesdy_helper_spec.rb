1c4b6fd6f0fc11b5a4eab72fc9e001b8d001f945
kaz-okamoto <kazu11518@gmail.com>
2014-07-12 09:57:07 +0900

    #11 modify helper's spec

--- spec/helpers/fesdy_helper_spec.rb
+++ spec/helpers/fesdy_helper_spec.rb
@@ -1,15 +1,6 @@
+# encoding: utf-8
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the FesdyHelper. For example:
-#
-# describe FesdyHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe FesdyHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe FesdyHelper, type: :helper do
+  pending '未使用'
 end
