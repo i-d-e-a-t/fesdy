633520338ea09f401d4c1d499b39d18b3df21817
kazu.okamoto <kazu11518@gmail.com>
2014-07-16 04:04:19 +0900

    #12 passed 'festival.setlist(artist)'

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -2,4 +2,8 @@
 class Festival < ActiveRecord::Base
   has_many :appearances
   has_many :artists, through: :appearances
+
+  def setlist(artist)
+    artist.setlist self
+  end
 end
--- spec/models/festival_spec.rb
+++ spec/models/festival_spec.rb
@@ -6,10 +6,13 @@ describe Festival, :type => :model do
       help_create_models_for_relations
       @festival = Festival.last
       @artist = Artist.last
+      @music = Music.last
     end
     it "#artistsで出演するアーティストを取得できる" do
       expect(@festival.artists).to eq [@artist]
     end
-    it "#setlist(artist)でセットリストを取得できる"
+    it "#setlist(artist)でセットリストを取得できる" do
+      expect(@festival.setlist(@artist)).to eq [@music]
+    end
   end
 end
