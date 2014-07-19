b33cbd3bff115cebee8e2488f4f8cf7a57bee4fe
Kaz Okamoto <kazu11518@gmail.com>
2014-07-20 08:50:22 +0900

    #36 added spec of FestivalsController

--- spec/controllers/festivals_controller_spec.rb
+++ spec/controllers/festivals_controller_spec.rb
@@ -4,12 +4,35 @@ require 'rails_helper'
 describe FestivalsController, :type => :controller do
 
   describe "GET 'show'" do
-    it "catches '/festivals/:festival_key' (like REST)"
-    it "doesn't catch '/festivals/show' (like RPC)"
+    before do
+      help_create_models_for_relations
+      @festival = Festival.last
+      @exist_key = @festival.path_key
+      @not_exist_key = 'it-is-not-festivals-key'
+    end
+
+    it "returns 200 with '/festivals/:exist_key'     request" do
+      expect(:get => "/festivals/#{@exist_key}").
+        to have_http_status(:ok)
+
+    end
+
+    it "returns 404 with '/festivals/:not_exist_key' request" do
+      expect(:get => "/festivals/#{@not_exist_key}").
+        to_have_http_status(:not_found)
+
+    end
+
+    it "doesn't catch '/festivals/show' (like RPC)" do
+      expect(:get => "/festivals/show").not_to be_routable
+
+    end
+
     it "returns http success" do
       get 'show'
       expect(response).to be_success
     end
+
   end
 
 end
