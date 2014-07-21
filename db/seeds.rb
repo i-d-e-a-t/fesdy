8ec61a0c391572f019fd1db614410ff153818bf6
craftpaperbag <kazu11518@gmail.com>
2014-07-21 22:37:08 +0900

    #36 modified db/seeds.rb (it is not best-way for associated-records)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # サマソニ2014
 #
 
-Festival.create(
+festival = Festival.create(
   path_key: 'summer-sonic-2014',
   name: 'サマーソニック2014',
   start_date: DateTime.new(2014, 8, 16),
@@ -20,12 +20,19 @@ Festival.create(
 #
 
 File.open(Rails.root.to_s + '/summer-sonic-2014.artists') do |f|
+  artist_id_count = 1
   while name = f.gets
     name.chomp!
-    artist = name.split("\t")
-    Artist.create(
-      name: artist[0],
-      path_key: artist[1]
+    a = name.split("\t")
+    artist = Artist.create(
+      name: a[0],
+      path_key: a[1]
     )
+    # summer-sonic-2014に出演する
+    Appearance.create(
+      festival_id: 1,
+      artist_id: artist_id_count
+    )
+    artist_id_count += 1
   end
 end
