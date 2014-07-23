5a0ecb67b192c7b88a220f1a16bf9e1629bdc015
craftpaperbag <kazu11518@gmail.com>
2014-07-23 23:20:53 +0900

    #36 add eyecatch effects :P

--- app/assets/javascripts/festivals.js
+++ app/assets/javascripts/festivals.js
@@ -1,5 +1,12 @@
 $(function() {
   // 画面描画完了時の処理
+  // アイキャッチ画像のフェードイン
+  $('.eye-catch').fadeOut(0).fadeIn('slow');
+  // フェス情報がふわっと登場する
+  $('.fes .info-box').animate({"opacity": "-=1", "top": "-=3rem"}, 0);
+  setTimeout(function() {
+    $('.fes .info-box').animate({"opacity": "+=1", "top": "+=3rem"}, 'slow');
+  }, 500);
 
   // button.to-topをクリックすると上に戻る
   $('button.to-top').click(function() {
