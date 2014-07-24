6fb7930c01adbd7a11ca9f0eb4965fabd3d2bbf8
craftpaperbag <kazu11518@gmail.com>
2014-07-24 21:28:44 +0900

    #36 fix bug (onload javascript)

--- app/assets/javascripts/festivals.js
+++ app/assets/javascripts/festivals.js
@@ -1,20 +1,20 @@
-$(function() {
-  // 画面描画完了時の処理
-  // アイキャッチ画像のフェードイン
+// onload script
+function ready_for_festival() {
+  // fade-in eyecatch image
   $('.eye-catch').fadeOut(0).fadeIn('slow');
-  // フェス情報がふわっと登場する
+  // fade-in & slide-in festival information
   $('.fes .info-box').animate({"opacity": "-=1", "top": "-=3rem"}, 0);
   setTimeout(function() {
     $('.fes .info-box').animate({"opacity": "+=1", "top": "+=3rem"}, 'slow');
   }, 500);
 
-  // button.to-topをクリックすると上に戻る
+  // when button.to-top clicked
   $('button.to-top').click(function() {
     $('body, html').animate(
       {scrollTop: 0},
-      // 0.5秒
+      // 0.5sec
       500
     );
     return false;
   });
-});
+};
