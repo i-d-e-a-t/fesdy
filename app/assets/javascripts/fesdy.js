5d3dd7ce8dee320a5554346763c4361033589ced
craftpaperbag <kazu11518@gmail.com>
2014-07-30 22:18:02 +0900

    #45 add navigation to top looks like 'cards'

--- app/assets/javascripts/fesdy.js
+++ app/assets/javascripts/fesdy.js
@@ -1,12 +1,8 @@
 // onload script
 function ready_for_fesdy() {
-  // fade-in welcome-card
-  $('.card.welcome').animate({
-    "opacity": "0",
-    "margin-top"   : "-2rem"
-  }, 0);
-  $('.card.welcome').animate({
-    "opacity": "1",
-    "margin-top"   : "0rem"
-  }, 1000);
+  // fade in
+  $(".card").each(function(index) {
+    $(this).fadeOut(0);
+    $(this).delay(200*index).fadeIn(300);
+  });
 };
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :prepare_for_header
+
+  private
+  def prepare_for_header
+    @festivals = Festival.all
+  end
 end
