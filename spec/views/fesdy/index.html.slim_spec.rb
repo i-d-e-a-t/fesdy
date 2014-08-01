c953b8da1ed8be967365fd3a67e6213d99dffdd1
craftpaperbag <kazu11518@gmail.com>
2014-08-01 22:12:59 +0900

    #66 add view spec & fix helper method

--- spec/views/fesdy/index.html.slim_spec.rb
+++ spec/views/fesdy/index.html.slim_spec.rb
@@ -2,4 +2,10 @@
 require 'rails_helper'
 
 describe "fesdy/index.html.slim", :type => :view do
+  # renderで描画。
+  # エラーがある場合例外が発生するのでテストに失敗する。
+  # エラーがない場合テストに成功する。
+  it "no error" do
+    render
+  end
 end
