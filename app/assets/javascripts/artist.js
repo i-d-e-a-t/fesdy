fdc41de5d3295447c65df346ef6606a058e80662
sosuke nakamura <nakasonoaddress@gmail.com>
2014-08-02 23:14:57 +0900

    refs #65 ArtistPageで表示する埋め込みYoutubeフレームのデザインを改善 (技術的な課題から、暫定で表示動画個数を1つに変更)
    modified:   app/assets/javascripts/artist.js
    modified:   app/views/artist/show.html.slim

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -1,5 +1,41 @@
 // onload script
 
 function ready_for_artist(){
-  // 空箱だけ準備しとく。何かすることできたら書く
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+
 }
+
+function onYouTubeIframeAPIReady() {
+
+  var video_id = $('#video-frame').data('video-id');
+  console.log('video-id = ' + video_id)
+
+  player = new YT.Player('video-frame', {
+    height: '390',
+    width: '640',
+    videoId: video_id,
+    playerVars:{
+      autohide: 0,
+      controls: 1,
+      showinfo: 0,
+      modestbranding: 1,
+      rel: 0,
+      theme: 'light',
+      color: 'white'
+    },  
+    events: {
+      'onReady': onPlayerReady,
+      // 'onStateChange': onPlayerStateChange
+    }   
+  }); 
+  function onPlayerReady(event) {
+    // event.target.playVideo();
+  }
+}
+
+
