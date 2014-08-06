45d00c7b0ed24f8108f30dac9c5a2847754ef039
craftpaperbag <kazu11518@gmail.com>
2014-08-06 22:10:20 +0900

    #94 add 'view has no error' spec

--- spec/views/festivals/study.html.slim_spec.rb
+++ spec/views/festivals/study.html.slim_spec.rb
@@ -1,5 +1,17 @@
+# encoding: utf-8
 require 'rails_helper'
 
-RSpec.describe "festivals/study.html.slim", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "festivals/study.html.slim", :type => :view do
+  before do
+    # 最低限のレコードを登録
+    help_create_models_for_relations
+    # コントローラーが設定するインスタンス変数をスタブ
+    assign(:artist, Artist.first)
+    assign(:artist_ary, Artist.all)
+    assign(:yt_video_ids, [1,2,3])
+  end
+
+  it "no error" do
+    render
+  end
 end
