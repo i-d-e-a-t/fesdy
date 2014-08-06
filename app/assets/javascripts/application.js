8fcb2609577065ab8fb55b150a514b6e9f5fabd7
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-06 17:27:53 +0900

    move method for youtube from artist to application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,70 @@ $(function() {
   // for iPhone
   $('thead').click(function() {});
 });
+
+function ready_for_ytpages() {
+
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+
+}
+
+function onYouTubeIframeAPIReady() {
+
+  var video_id = $('#video-frame').data('video-id');
+
+  player = new YT.Player('video-frame', {
+    height: '390',
+    width: '640',
+    videoId: video_id,
+    playerVars:{
+      autohide: 0,
+      controls: 1,
+      showinfo: 0,
+      modestbranding: 1,
+      rel: 0,
+      theme: 'light',
+      color: 'white'
+    },  
+    events: {
+      'onReady': onPlayerReady,
+     // 'onStateChange': onPlayerStateChange
+    }   
+  }); 
+  
+  function onPlayerReady(event) {
+    // event.target.playVideo();
+  }
+/*
+ * このコメントアウトを外すとyoutubeも読み込まれなくなった
+ *
+  function onPlayerStateChange(e) {
+    if (e.data == YT.PlayerState.ENDED) {
+      playEnd();
+    }
+    if (e.data == YT.PlayerState.PLAYING) {
+      playBtn.hide();
+    }
+  }
+
+  function playEnd() {
+    //再生が終わったら次の曲のidを調べる
+    function() {
+      $.ajax({
+        url: "next_song",
+        type: "GET",
+        dataType: "html"
+      });
+    }
+    layer.clearVideo();
+    
+    player.loadVideoById($('#video-frame').data('video-id'));
+    player.playVide();
+  }
+*/
+
+}
+
