01795201c973d4fc8be9dee96149baf0663ac5cf
craftpaperbag <kazu11518@gmail.com>
2014-08-13 20:59:11 +0900

    #95 add some test for /festival/.../study

--- spec/controllers/festivals_controller_spec.rb
+++ spec/controllers/festivals_controller_spec.rb
@@ -34,4 +34,37 @@ describe FestivalsController, :type => :controller do
 
   end
 
+  describe "GET study" do
+    context "フェスごとの予習(セッション情報なし)" do
+      before do
+        help_create_models_for_relations
+        @festival = Festival.last
+        @exist_key = @festival.path_key
+        @not_exist_key = 'it-is-not-festivals-key'
+        session.each { |k, v| session[k] = nil }
+      end
+
+      it "success with '/festivals/:exist_key/study'" do
+        get :study, id: @exist_key
+        expect(response.status).to eq 200
+        expect(response).to render_template("study")
+      end
+      it "returns 404 with '/festivals/:not_exist_key/study'" do
+        get :study, id: @not_exist_key
+        expect(response.status).to eq 404
+        expect(session[:study_id]).to be nil
+        expect(session[:study_list]).to be nil
+      end
+      it "セッションに予習情報をセットする" do
+        get :study, id: @exist_key
+        expect(response.status).to eq 200
+        expect(response).to render_template("study")
+        expect(session[:study_id]).to eq @exist_key.to_s
+        expect(session[:study_list].class).to be Array
+        # このテストでは１件だけなので、sessionにはから配列が入る
+        expect(session[:study_list].length).to eq 0
+      end
+    end
+  end
+
 end
