f6b9b720d88777fd67d60920e7480f3ebf50fa0d
craftpaperbag <kazu11518@gmail.com>
2014-08-14 23:41:47 +0900

    #95 refactor spec (group shared examples)

--- spec/controllers/festivals_controller_spec.rb
+++ spec/controllers/festivals_controller_spec.rb
@@ -43,7 +43,57 @@ describe FestivalsController, :type => :controller do
       @date = FestivalDate.last
       @exist_date_key = @date.path_key
       @not_exist_date_key = 'it-is-not-festival-dates-key'
+      @last_session = session.dup
     end
+
+    # 共通テスト
+    shared_examples_for 'start study' do
+      it "成功" do
+        expect(response.status).to eq 200
+      end
+      it "studyを描画" do
+        expect(response).to render_template("study")
+      end
+      it "セッションに予習IDをセットする" do
+        expect(session[:study_id]).to eq @exist_key.to_s
+      end
+      it "セッションに予習リストをセットする" do
+        expected_length = @festival.artists.count < 10 ? @festival.artists.count - 1 : 9
+        expect(session[:study_list].length).to eq expected_length
+      end
+      it "セッションに次のアーティストの名前が入っている" do
+        expect(@festival.artists.all.pluck(:name)).to include session[:study_next_artist]
+      end
+    end
+    shared_examples_for 'start study failed' do
+      it "失敗" do
+        expect(response.status).not_to be :success
+      end
+      it "セッションに予習IDをセットしない" do
+        expect(session[:study_id]).to be nil
+      end
+      it "セッションに予習リストをセットしない" do
+        expect(session[:study_list]).to be nil
+      end
+      it "セッションに次のアーティストの名前が入っていない" do
+        expect(session[:study_next_artist]).to be nil
+      end
+    end
+    shared_examples_for 'restart study failed' do
+      it "失敗" do
+        expect(response.status).not_to be :success
+      end
+      it "セッションの予習IDが変化しない" do
+        expect(session[:study_id]).to eq @last_session[:study_id]
+      end
+      it "セッションの予習リストが変化しない" do
+        expect(session[:study_list]).to eq @last_session[:study_list]
+      end
+      it "セッションの次のアーティストの名前が変化しない" do
+        expect(session[:study_next_artist]).to eq @last_session[:study_next_artist]
+      end
+    end
+
     #
     # /festivals/:id/study
     #
@@ -52,54 +102,39 @@ describe FestivalsController, :type => :controller do
         before do
           session.each { |k, v| session[k] = nil }
         end
-
-        it "success with '/festivals/:exist_key/study'" do
-          get :study, id: @exist_key
-          expect(response.status).to eq 200
-          expect(response).to render_template("study")
+        context "存在するフェスにリクエストを送ると" do
+          before do
+            get :study, id: @exist_key
+          end
+          it_behaves_like 'start study'
         end
-        it "returns 404 with '/festivals/:not_exist_key/study'" do
-          get :study, id: @not_exist_key
-          expect(response.status).to eq 404
-          expect(session[:study_id]).to be nil
-          expect(session[:study_list]).to be nil
-        end
-        it "セッションに予習情報をセットする" do
-          get :study, id: @exist_key
-          expect(session[:study_id]).to eq @exist_key.to_s
-          expected_length = @festival.artists.count < 10 ? @festival.artists.count - 1 : 9
-          expect(session[:study_list].length).to eq expected_length
-        end
-        it "セッションに次のアーティストの名前が入っている" do
-          get :study, id: @exist_key
-          expect(@festival.artists.all.pluck(:name)).to include session[:study_next_artist]
+        context "存在しないフェスにリクエストを送ると" do
+          before do
+            get :study, id: @not_exist_key
+          end
+          it "404を返却" do
+            expect(response.status).to eq 404
+          end
+          it_behaves_like 'start study failed'
         end
       end
       context "最後の１アーティストの場合、" do
         before do
           session[:study_id] = @exist_key
           session[:study_list] = []
+          session[:study_next_artist] = nil
         end
-        it "success with '/festivals/:exist_key/study'" do
-          get :study, id: @exist_key
-          expect(response.status).to eq 200
-          expect(response).to render_template("study")
+        context "存在するフェスにリクエストを送ると" do
+          before do
+            get :study, id: @exist_key
+          end
+          it_behaves_like 'start study'
         end
-        it "returns 404 with '/festivals/:not_exist_key/study'" do
-          get :study, id: @not_exist_key
-          expect(response.status).to eq 404
-          expect(session[:study_id]).to eq @exist_key
-          expect(session[:study_list]).to eq []
-        end
-        it "セッションにあらたな予習情報をセットする" do
-          get :study, id: @exist_key
-          expect(session[:study_id]).to eq @exist_key.to_s
-          expected_length = @festival.artists.count < 10 ? @festival.artists.count - 1 : 9
-          expect(session[:study_list].length).to eq expected_length
-        end
-        it "セッションに次のアーティストの名前が入っている" do
-          get :study, id: @exist_key
-          expect(@festival.artists.all.pluck(:name)).to include session[:study_next_artist]
+        context "存在しないフェスにリクエストを送ると" do
+          before do
+            get :study, id: @not_exist_key
+          end
+          it_behaves_like 'restart study failed'
         end
       end
     end
