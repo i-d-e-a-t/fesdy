c1d2f0c30c09607504b88dd676c6ca0a5d35393e
craftpaperbag <kazu11518@gmail.com>
2014-08-16 00:39:04 +0900

    fix bug(ビューのテスト修正漏れ) #125

--- spec/views/festivals/study.html.slim_spec.rb
+++ spec/views/festivals/study.html.slim_spec.rb
@@ -6,8 +6,10 @@ describe "festivals/study.html.slim", :type => :view do
     # 最低限のレコードを登録
     help_create_models_for_relations
     # コントローラーが設定するインスタンス変数をスタブ
-    assign(:artist, Artist.first)
-    assign(:artist_ary, Artist.all)
+    fes = Festival.last
+    artist = fes.artists.last
+    assign(:target, fes)
+    assign(:artist, artist)
     assign(:yt_video_ids, [1,2,3])
   end
 
