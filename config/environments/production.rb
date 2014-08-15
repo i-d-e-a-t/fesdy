8058405f1964bc9e04d39dc2b27dbedfadaa6b2d
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-15 19:19:32 +0900

    add Google Tracker ID

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Google analytics tracker
+  GA.tracker = "UA-53852322-1"
 end
