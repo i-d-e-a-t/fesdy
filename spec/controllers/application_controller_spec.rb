e747c18cf7c04d6a5c4de0c840e9d30a83eff191
craftpaperbag <kazu11518@gmail.com>
2014-08-20 00:34:40 +0900

    add spec for youtube-method(RED) #125

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe ApplicationController, :type => :controller do
+describe ApplicationController, :type => :controller, :youtube => true do
+  let(:max) { ApplicationController::NUM_OF_LIST }
+  describe '#get_yt_video_ids' do
+    context '「柴犬」を引数に' do
+      before :all do
+        @parameter = '柴犬'
+      end
+      context '検索した時' do
+        subject {
+          ApplicationController.new.send(:get_yt_video_ids, @parameter)
+        }
+        its(:class) { should eq Array }
+        its(:length) { should be <= max }
+      end
 
+      context '１回検索済みで、再度検索した時は' do
+        before :all do
+          @ids = ApplicationController.new.send(:get_yt_video_ids, @parameter)
+        end
+        subject {
+          ApplicationController.new.send(:get_yt_video_ids, @parameter)
+        }
+        its(:class) { should eq Array }
+        its(:length) { should be <= max }
+        it { is_expected.not_to eq @ids }
+        its(:first) { is_expected.not_to eq @ids.first }
+        its(:length) { is_expected.not_to eq @ids.length }
+        its(:last) { is_expected.not_to eq @ids.last }
+      end
+    end
+  end
 end
