f17de2b51ad6ca4e577661402d7738d36b62ef32
craftpaperbag <kazu11518@gmail.com>
2014-08-20 00:47:04 +0900

    shuffle youtube result #125

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,11 @@ class ApplicationController < ActionController::Base
     @festivals = Festival.all
   end
 
-  # search_wordをキーにYoutube検索を行い、Video-Idを最大3件配列で返却
+  # search_wordをキーにYoutube検索を行い、Video-Idを最大NUM_OF_MAX件配列で返却
   DEVELOPER_KEY = secret :youtube_apikey
   YOUTUBE_API_SERVICE_NAME = "youtube"
   YOUTUBE_API_VERSION = "v3"
+  NUM_OF_MAX = 10
   NUM_OF_LIST = 3
 
   def get_yt_video_ids(search_word)
@@ -33,7 +34,7 @@ class ApplicationController < ActionController::Base
     # Optionの設定(検索キーワード/取得数)
     opts = Trollop::options do
       opt :q, 'Search term', :type => String, :default => search_word
-      opt :maxResults, 'Max results', :type => :int, :default => NUM_OF_LIST
+      opt :maxResults, 'Max results', :type => :int, :default => NUM_OF_MAX
     end
 
     # 検索実行
@@ -51,7 +52,7 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    return yt_video_ids
+    return yt_video_ids.shuffle.slice(0, NUM_OF_LIST)
   end
 
 end
