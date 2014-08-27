db03bc3b58c125d1a61a1ba59cb8ca53c69a45ef
craftpaperbag <kazu11518@gmail.com>
2014-08-27 23:38:38 +0900

    refactor code (move Festival's validates upon methods definitions)

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -3,6 +3,11 @@ class Festival < ActiveRecord::Base
   has_many :appearances, through: :festival_dates
   has_many :artists, through: :appearances
 
+  # 同じpath_keyは登録不可
+  validates :path_key,
+    presence: true,
+    uniqueness: true
+
   # 画面に表示しても違和感のない、このフェスを表す文字列を返却。
   def to_title
     name
@@ -21,9 +26,4 @@ class Festival < ActiveRecord::Base
   # フェス開催の場合は詳細を出す。
   # 互換性のためnilを返却
   def to_detail_for_title; end
-
-  # 同じpath_keyは登録不可
-  validates :path_key,
-    presence: true,
-    uniqueness: true
 end
