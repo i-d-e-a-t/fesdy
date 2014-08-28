a703b778233139d2ff07221c1058b86f2628428c
Takahiro Uchiyama <hochweit728@gmail.com>
2014-08-28 12:26:08 +0900

    modify scraping scipt, create love shower artists

--- lib/script/love-shower-2014.scrape.rb
+++ lib/script/love-shower-2014.scrape.rb
@@ -4,6 +4,7 @@
 require 'nokogiri'
 require 'open-uri'
 require 'FileUtils'
+require 'pry'
 
 # パスキー作成モジュールをインクルード
 require_relative '../tasks/ask_path_key.rb'
@@ -34,10 +35,46 @@ def scrape_artists apk, url, file = nil
   end
 
   # HTMLからartist名を取得
-  result_829 = doc.css '#sec01 ul.profList.btn li'
+  result_829 = doc.css '#sec01 ul.profList.btn li img'
+  artists_829 = []
+  result_829.each do | r |
+    artists_829 << r['alt']
+  end
 
-  # test
-  p result_829
+  # Pathキーを取得して、[artist名 path_key 日付]でファイル出力
+  artists_829.each do | artist |
+    name = artist.to_s
+    tmp =apk.ask name
+    f.puts tmp[0] + "\t" + tmp[1] + "\t" + "20140829"
+  end
+  
+  # HTMLからartist名を取得
+  result_830 = doc.css '#sec02 ul.profList.btn li img'
+  artists_830 = []
+  result_830.each do | r |
+    artists_830 << r['alt']
+  end
+
+  # Pathキーを取得して、[artist名 path_key 日付]でファイル出力
+  artists_830.each do | artist |
+    name = artist.to_s
+    tmp =apk.ask name
+    f.puts tmp[0] + "\t" + tmp[1] + "\t" + "20140830"
+  end
+
+  # HTMLからartist名を取得
+  result_831 = doc.css '#sec03 ul.profList.btn li img'
+  artists_831 = []
+  result_831.each do | r |
+    artists_831 << r['alt']
+  end
+
+  # Pathキーを取得して、[artist名 path_key 日付]でファイル出力
+  artists_831.each do | artist |
+    name = artist.to_s
+    tmp =apk.ask name
+    f.puts tmp[0] + "\t" + tmp[1] + "\t" + "20140831"
+  end
 
   f.close
 end
@@ -58,14 +95,21 @@ old_files = []
 files.each do |fn|
   if File.exists? fn
     of = fn + '.old'
-    old_files << of 
+    old_files << of
     # ファイル名にoldをつけて退避する。
-    railse "ファイルコピーに失敗: #{file}" if FileUtils.cp(fn, of)
+    raise "ファイルコピーに失敗: #{file}" if FileUtils.cp(fn, of)
   end
 end
 
 additional_rules = {
 }
 apk = AskPathKey.new additional_rules
+# 履歴を登録
+old_files.each { |of| apk.load_history of }
 
 scrape_artists apk, love_shower_url, LOVE_SHOWER_OUTNAME 
+
+# 退避したファイルを削除
+old_files.each do |of|
+  File.delete of if File.exists? of
+end
