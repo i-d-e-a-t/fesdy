ced74ef10f72ad696a8ba1af84afbb247e5828f1
Takahiro Uchiyama <hochweit728@gmail.com>
2014-09-02 18:03:13 +0900

    Remove artist.path_key

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -6,7 +6,7 @@ class ArtistController < ApplicationController
   #
   def show
     # TODO: path_key使う
-    @artist = Artist.where(:path_key => params[:id]).last
+    @artist = Artist.where(:id => params[:id]).last
     if @artist != nil
       @yt_video_ids = get_yt_video_ids(@artist.name)
     else
