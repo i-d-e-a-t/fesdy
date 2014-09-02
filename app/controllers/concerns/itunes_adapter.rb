3ad7085a9d021c9a5a13d57f2f90735503bebcc0
craftpaperbag <kazu11518@gmail.com>
2014-09-02 22:26:28 +0900

    ItunesAdapter.search returns Hash

--- app/controllers/concerns/itunes_adapter.rb
+++ app/controllers/concerns/itunes_adapter.rb
@@ -6,9 +6,23 @@ module ItunesAdapter
     search: "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch"
   }
 
-  def self.search keyword, options={}
-    # キーワードを引数に検索
+  # キーワードで検索した結果をHashWithIndifferentAccessオブジェクトで返す。
+  #     option == :raw の場合
+  #         結果のJSONをそのまま返却する
+  def self.search keyword, option=nil
+    raw = self.request_search keyword
+    if option == :raw
+      return raw
+    else
+      return JSON.parse(raw).with_indifferent_access
+    end
+  end
+
+  # キーワードを引数に検索
+  # 結果（JSON）をテキストで返却する
+  def self.request_search keyword
     http_client = HTTPClient.new
     http_client.get_content(ITUNES_API[:search], {term: keyword})
   end
+
 end
