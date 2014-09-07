8bd511fc3aaef8ceb47120b887316e81eca7845c
Takahiro Uchiyama <hochweit728@gmail.com>
2014-09-07 21:54:22 +0900

    Make dog quiet

--- spec/controllers/artist_controller_spec.rb
+++ spec/controllers/artist_controller_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
-describe ArtistController, :type => :controller do
+describe ArtistController, type: :controller do
 
   describe "GET 'show'" do
 
     before do
       help_create_models_for_relations
-      @artist= Artist.last
+      @artist = Artist.last
       @exist_id = @artist.id
       @not_exist_id = '99999'
-    end 
+    end
 
     it "success with '/artists/:exist_id'" do
       get :show, id: @exist_id
-      expect(response.status).to eq 200 
-      expect(response).to render_template("show")
+      expect(response.status).to eq 200
+      expect(response).to render_template('show')
     end
-    
+
     it "return 404 with '/artists/:not_exist_id'" do
       get :show, id: @not_exist_id
       expect(response.status).to eq 404
@@ -24,22 +24,20 @@ describe ArtistController, :type => :controller do
 
   end
 
-  describe "#search_itunes" do
+  describe '#search_itunes' do
     before do
       help_create_models_for_relations
-      @artist= Artist.last
+      @artist = Artist.last
       @exist_id = @artist.id
       @not_exist_key = '999999'
-    end 
+    end
     it "catches '/artists/.../itunes'" do
-      expect(:get => "/artists/#{@exist_id}/itunes").to route_to(
+      expect(get: "/artists/#{@exist_id}/itunes").to route_to(
         controller: 'artist',
-            action: 'search_itunes',
-                id: "#{@exist_id}"
+        action: 'search_itunes',
+        id: "#{@exist_id}"
       )
     end
   end
 
-
 end
-
