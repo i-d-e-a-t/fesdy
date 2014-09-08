933c4bcab6115c5e2ba1e93b34a511ec19418215
craftpaperbag <kazu11518@gmail.com>
2014-09-08 21:56:16 +0900

    modulize ScrapeHelper

--- lib/script/scrape_helper.rb
+++ lib/script/scrape_helper.rb
@@ -2,36 +2,42 @@ require 'nokogiri'
 require 'open-uri'
 require 'FileUtils'
 
-# urlとcssを指定するruleを渡してスクレイプする
-# (xpathでスクレイプする場合は要検討)
-def scrape_with_nokogiri(url, rule)
-  begin
-    doc = Nokogiri::HTML(open url)
-  rescue => e
-    puts "#{e.class}"
-    puts e.message
-    exit 1
+# フェス情報をスクレイピングして、
+# artistsファイルに出力する手助けをするモジュール。
+#
+# 本ファイルをrequireすることで
+# 自動的にincludeされるので注意
+module ScrapeHelper
+  # urlとcssを指定するruleを渡してスクレイプする
+  # (xpathでスクレイプする場合は要検討)
+  def scrape_with_nokogiri(url, rule)
+    begin
+      doc = Nokogiri::HTML(open url)
+    rescue => e
+      puts "#{e.class}"
+      puts e.message
+      exit 1
+    end
+
+    doc.css rule
   end
 
-  result = doc.css rule
-end
+  # ファイル名、アーティストの配列、日付を渡して
+  # ファイル出力する
+  def print_to_file(file, artists, date)
+    f = File.open(file, 'a')
 
-# ファイル名、アーティストの配列、日付を渡して
-# ファイル出力する
-def print_to_file(file, artists, date)
-  f = File.open(file, 'a')
+    artists.each_with_index do |r|
+      name = r.to_s
+      f.puts name + "\t" + date
+    end
 
-  artists.each_with_index do |r|
-    name = r.to_s
-    f.puts name + "\t" + date
+    f.close
   end
 
-  f.close
-end
-
-# 既にファイルが存在していたときの履歴管理
-def stash_old_file(file)
-  if File.exist? file
+  # 既にファイルが存在していたときの履歴管理
+  def stash_old_file(file)
+    return unless File.exist? file
     of = file + '.old'
     begin
       File.rename(file, of)
@@ -40,10 +46,15 @@ def stash_old_file(file)
       puts e.message
     end
   end
+
+  # 既に存在していたファイルを削除
+  def delete_old_file(file)
+    old_fn = file + '.old'
+    File.delete old_fn if File.exist? old_fn
+  end
 end
 
-# 既に存在していたファイルを削除
-def delete_old_file(file)
-  old_fn = file + '.old'
-  File.delete old_fn if File.exist? old_fn
-end
+#
+# 自動include
+#
+include ScrapeHelper
